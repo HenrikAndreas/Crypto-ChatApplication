fb69d45de0658f934a11da6e16aa5d4fcf699d07
HenrikAndreas <henrikbratli@live.no>
2020-10-10 18:55:59 +0200

    Added files

---
+++ main.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, send
+from Crypto.Cipher import AES
+from Crypto import Random
+# from base64 import b64decode, b64encode
+import base64
+import os
+
+#Cryptograhy --Start-- Credz to JoExBaYeR
+crypto_key = 'henrikandreasbra'
+BLOCK_SIZE = 16
+def pad(data):
+    length = 16 - (len(data) % 16)
+    return data + chr(length)*length
+
+def unpad(data):
+    return data[:-ord(data[-1])]
+
+def encrypt(message, passphrase):
+    IV = Random.new().read(BLOCK_SIZE)
+    aes = AES.new(passphrase.encode(), AES.MODE_CFB, IV, segment_size=128)
+    return base64.b64encode(IV + aes.encrypt(pad(message).encode()))
+
+def decrypt(encrypted, passphrase):
+    encrypted = base64.b64decode(encrypted)
+    
+    IV = encrypted[:BLOCK_SIZE]
+    aes = AES.new(passphrase.encode('utf-8'), AES.MODE_CFB, IV, segment_size=128)
+    return (aes.decrypt(encrypted[BLOCK_SIZE:]))
+#Cryptography --End--
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(64) # 64 random bytes
+socketio = SocketIO(app)
+
+# include request.sid
+usersOnline = []
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+
+def sendHistory():
+    path = os.path.relpath('data\\log.dat', os.path.dirname(__file__))
+    logFile = open(path, 'r')
+    data = []
+    for line in logFile:
+        currentLine = line.lstrip().rstrip()
+        currentLine = encrypt(currentLine, crypto_key).decode()
+        data.append(currentLine)
+    emit('connection', data)
+
+@socketio.on('signUp')
+def registrationHandler(username, password):
+    path = os.path.relpath('data\\users.dat', os.path.dirname(__file__))
+    userFile = open(path, 'a')
+    userFile.write("{0:s} : {1}\n".format(username, password))
+    userFile.close()
+    emit('signUp')
+    sendHistory()
+
+@socketio.on('login')
+def loginHandler(username, password):
+    path = os.path.relpath('data\\users.dat', os.path.dirname(__file__))
+    userFile = open(path, 'r')
+    io_id = request.sid
+    for l in userFile:
+        line = l.split(':')
+        line[0] = line[0].lstrip().rstrip()
+        line[1] = line[1].lstrip().rstrip()
+        if username.lower() == line[0].lower():
+            if password == line[1]:
+                emit('login', 'ok') #Weakest link?
+                sendHistory()
+    userFile.close()
+
+
+@socketio.on('sendMessage')
+def messageHandler(message): # From client
+    message = decrypt(message, crypto_key)
+    message = message.decode('unicode_escape')
+    message = unpad(message)
+    path = os.path.relpath('data\\log.dat', os.path.dirname(__file__))
+    logFile = open(path, 'a')
+    logFile.write("{0:s}\n".format(message))
+    logFile.close()
+
+    emit('messageHandler', encrypt(message, crypto_key).decode(), broadcast=True)
+
+def messageFromServer(message):
+    path = os.path.relpath('data\\log.dat', os.path.dirname(__file__))
+    logFile = open(path, 'a')
+    logFile.write("{0:s}\n".format(message))
+    message = encrypt(message, crypto_key).decode()
+    emit('messageHandler', message, broadcast=True)
+
+
+@socketio.on('disconnect_user')
+def disconnection(username):
+    try:
+        usersOnline.remove(username)
+        user = encrypt(username, crypto_key).decode()
+        emit('remove_user', user, broadcast=True)
+        messageFromServer("{0:s} has disconnected!".format(username))
+    except:
+        print("[SERVER] Error on disconnect")
+
+
+@socketio.on('username_distribution')
+def username_distribution(username):    
+    usersOnline.append(username)
+    users = usersOnline[:]
+    for i in range(len(users)):
+        users[i] = encrypt(users[i], crypto_key).decode()
+
+    emit('username_distribution', users, broadcast=True)
+    messageFromServer("{0:s} has connected!".format(username))
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="127.0.0.1", port=80, debug=False)
---
+++ static/main.js
@@ -0,0 +1,179 @@
+// TODO 
+// Fix --> SignUp joining
+
+const socket = io.connect("127.0.0.1:80");
+
+let vueApp = new Vue({
+    el: '#vueApp',
+    data: {
+        title: "Chat Application",
+        username: '',
+        password: '',
+        showForm: false,
+        signUpVar: false, //Logic-var for login or sign up site
+        message: '',
+        data: null,
+        users: null,
+        decryption: false,
+        crypto_key: '',
+        loggedIn: false,
+        crypto_string: 'joe',
+        crypto_enc_string: 'IEfjW8eJaeXjfio29nCrJUCBDB1AsEbM9mcpkXRM2zU==='
+    },
+
+    methods: {
+        login: function() {
+            if ((this.username != '' & this.password != '')) {
+                // Encrypting password with SHA256
+                var pswd = forge.md.sha256.create().update(this.password).digest().toHex();
+                socket.emit('login', this.username, pswd);
+            } else {
+                console.log("Wrong password");
+            }
+        },
+        signUp: function() {
+            if (this.username != '' & this.password != '') {
+                // Encrypting password with SHA256
+                var pswd = forge.md.sha256.create().update(this.password).digest().toHex();
+                socket.emit('signUp', this.username, pswd);
+
+            } else {
+                console.log('Username and password dont match');
+            }
+        },
+        change_signup: function() {
+            if (this.signUpVar == false) {
+                this.signUpVar = true;
+            } else {
+                this.signUpVar = false;
+            }
+            this.password = '';
+            this.username = '';
+        },
+        sendMessage: function() {
+            var msg = (this.username + ": " + this.message);
+            socket.emit('sendMessage', this.encrypt(msg));
+            
+            this.message = '';
+        },
+        decryptionTester: function() {
+            if (this.crypto_string == this.decrypt(this.crypto_enc_string)) {
+                this.users = this.decrypt_array(this.users);
+                this.data = this.decrypt_array(this.data);
+                vueApp.$forceUpdate();
+                this.decryption = true;
+            } else {
+                console.log("Incorrect key!");
+            }
+
+        },
+        decrypt_array: function(array) {
+            for (var i = 0; i < array.length; i++) {
+                array[i] = this.decrypt(array[i]);
+            }
+            return array;
+        },
+        decrypt: function(encrypted_message) {
+            // console.log(encrypted_message);
+            var base64ciphertextFromPython = encrypted_message;
+            var ciphertext = CryptoJS.enc.Base64.parse(base64ciphertextFromPython);
+        
+            // split iv and ciphertext
+            var iv = ciphertext.clone();
+            iv.sigBytes = 16;
+            iv.clamp();
+            ciphertext.words.splice(0, 4); // delete 4 words = 16 bytes
+            ciphertext.sigBytes -= 16;
+        
+            // TEST SENERE UTEN PADDING -- IDIOT --
+            var key = CryptoJS.enc.Utf8.parse(this.crypto_key);
+        
+            // decryption
+            var decrypted = CryptoJS.AES.decrypt({ciphertext: ciphertext}, key, {
+              iv: iv,
+              mode: CryptoJS.mode.CFB
+            });
+            
+            return ( decrypted.toString(CryptoJS.enc.Utf8));
+
+        },
+        encrypt: function(message) {
+            var key = CryptoJS.enc.Utf8.parse(this.crypto_key);
+            var iv = CryptoJS.lib.WordArray.random(16);
+            var aes = CryptoJS.AES.encrypt(message, key, {iv: iv, mode: CryptoJS.mode.CFB});
+            var encrypted_message = CryptoJS.enc.Utf8.parse("");
+            encrypted_message.concat(iv);
+            encrypted_message.concat(aes.ciphertext);
+            return (CryptoJS.enc.Base64.stringify(encrypted_message));
+
+        }
+    }
+});
+
+
+
+socket.on('messageHandler', function(msg){
+    if (vueApp.loggedIn) {
+        if (vueApp.decryption) {
+            vueApp.data.push(vueApp.decrypt(msg));
+        } else {
+            vueApp.data.push(msg);
+        }
+    }
+});
+
+socket.on('connection', function(messages){
+    vueApp.data = messages;
+});
+
+
+socket.on('username_distribution', function(users){
+    if (vueApp.loggedIn) {
+        if (vueApp.decryption) {
+            vueApp.users = [];
+            for (var i = 0; i<users.length; i++) {
+                vueApp.users.push(vueApp.decrypt(users[i]));
+                console.log(vueApp.users[i]);
+            }
+        } else {
+            vueApp.users = users;
+        }
+    
+        // Auto scroll down
+        var objDiv = document.getElementById("chatDiv");
+        objDiv.scrollTop = objDiv.scrollHeight;
+
+    }
+});
+
+socket.on('remove_user', function(username) {
+    if (vueApp.loggedIn) {
+        username = vueApp.decrypt(username);
+        for(var i = 0; i < vueApp.users.length; i++) {
+            if (vueApp.users[i] == username) {
+                vueApp.users.splice(i, 1);
+            }
+        }
+    }
+});
+
+socket.on('signUp', function() {
+    vueApp.showForm = true;
+    vueApp.loggedIn = true;
+    socket.emit('username_distribution', vueApp.username);
+});
+
+socket.on('login', function(status) {
+    if (status == 'ok') {
+        vueApp.showForm = true;
+        vueApp.loggedIn = true;
+        socket.emit('username_distribution', vueApp.username);
+    }
+});
+
+window.onbeforeunload = function() {
+    if(vueApp.username != "") {
+        socket.emit('disconnect_user', vueApp.username);
+    }
+}
+
